bc35c9dea967cc5bbe0046d7f7ea1f2f43fa3b0e
Jobith M Basheer <jobith@three38inc.com>
2023-12-22 13:32:28 +0530

    added faker and scc changes for lists

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,3 +1,22 @@
 import '../styles/main.scss'
 import 'remixicon/fonts/remixicon.css'
 
+import Contact from './Contact'
+import { faker } from '@faker-js/faker';
+
+let contacts = []
+
+//new Contact('John', 'Doe', 25, true)
+for( let i = 0; i < 10; i++) {
+    contacts.push(
+        new Contact(
+            faker.person.firstName(),
+            faker.person.lastName(),
+            faker.image.avatar(),
+            faker.datatype.boolean(0.75)
+        )
+    )
+}
+
+console.log(contacts);
+
